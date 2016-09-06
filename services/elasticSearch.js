e48b344cfbc6647acdc8162a16cf1dc004ec89f2
neel-hektor <sherlockh64@gmail.com>
2016-09-06 15:24:48 +0530

    1. updating config samples 2. updating git ignore files 3. removing references to non nconf based configuration loading

--- services/elasticSearch.js
+++ services/elasticSearch.js
@@ -1,22 +1,23 @@
 const ES = require('elasticsearch');
+const config = require('../applicationContext');
 
-const esConfig = global.config.app.es;
+const esConfig = config.get('es');
 
 const ESClient = new ES.Client({
-  host: [esConfig.host, esConfig.port].join(':')
+    host: [esConfig.host, esConfig.port].join(':')
 });
 
 ESClient.ping({
-      requestTimeout: Infinity,
-      hello: "Elasticsearch!"
+        requestTimeout: Infinity,
+        hello: "Elasticsearch!"
     },
     function (error) {
-      if (error) {
-        console.trace('ElasticSearch cluster is down!');
-        process.exit(1);
-      } else {
-        console.log('ElasticSearch is available');
-      }
+        if (error) {
+            console.trace('ElasticSearch cluster is down!');
+            process.exit(1);
+        } else {
+            console.log('ElasticSearch is available');
+        }
     });
 
 module.exports = ESClient;
